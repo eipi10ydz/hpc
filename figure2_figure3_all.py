c8e597ff6f99bed9916f839c5330e546df6dfeb1
ydz <gdzjydz@mail.ustc.edu.cn>
2016-10-06 15:36:20 +0800

    correct a mistake in regular expression

--- figure2_figure3_all.py
+++ figure2_figure3_all.py
@@ -6,5 +6,5 @@ from multiprocessing import Process
 
 if __name__ == '__main__':
     file = next(os.walk('.'))[2]
-    file_need = [x for x in file if re.findall(r'([(AP_LB_)(AP_)(Naive_)]\d{2,3}?)[^(.png)]$', x)]
+    file_need = [x for x in file if re.findall(r'(AP_LB_|AP_|Naive_)\d{1,3}$', x)]
     list(map(lambda x : Process(target = os.execlp, args = ('python3', 'python3',  'figure2_figure3_impl.py', x, x + '_2', x + '_3')).start(), file_need))
